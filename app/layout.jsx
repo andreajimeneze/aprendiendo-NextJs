f0c2686bc736462c3c914e117203b32621bfa33f
Andrea Jota <andreacjimenez@gmail.com>
2023-06-15 19:56:57 -0400

    funcionalidad menú

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-no-comment-textnodes */
 
 import { Navigation } from '@/app/components/Navigation'
 
@@ -11,12 +12,14 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <link rel="stylesheet" href="https://bootswatch.com/5/flatly/bootstrap.min.css" />
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossOrigin="anonymous" type="module" defer />
+
       </head>
       <body>
         <Navigation />
         <div className='container p-4'>
         {children}
-        </div>
+        </div>       
       </body>
     </html>
   )
